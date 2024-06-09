f13627078bc8b16f0b5a70d372a3a262566ad8a8
Flor_Loto <florloto.ge@gmail.com>
2024-06-09 13:19:54 -0300

    Inicio del proyecto

---
+++ movies.py
@@ -0,0 +1,10 @@
+import os
+
+class Create_movie:
+    def __init__(self, genero, titulo, director, anio_estreno):
+        self.genero = genero
+        self.titulo = titulo
+        self.director = director
+        self.anio_estreno = anio_estreno
+        
+    
